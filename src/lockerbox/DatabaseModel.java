6c57b615d8ce7fea742630e41700cc2899938768
Jozi <jozigila@cit.edu.al>
2018-02-16 17:43:29 +0100

    Auto create database and table if it doesn't exist

--- src/lockerbox/DatabaseModel.java
+++ src/lockerbox/DatabaseModel.java
@@ -2,6 +2,7 @@ package lockerbox;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.Statement;
 
 public class DatabaseModel {
     public static Connection getConnection() throws Exception{
@@ -16,5 +17,31 @@ public class DatabaseModel {
         String dbPass = "";
         
         return DriverManager.getConnection(url, dbUser, dbPass);
+    }
+    
+    public static void initDatabase() throws Exception {
+        String driverName = "org.gjt.mm.mysql.Driver";
+        Class.forName(driverName);
+        
+        String serverName = "localhost";
+        String databaseName = "jozi";
+        
+        String url = "jdbc:mysql://" + serverName + "/?autoReconnect=true&useSSL=false&user=root&password=";
+        Connection conn = DriverManager.getConnection(url);
+        
+        String[] tableStatements = {
+            "users (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, username TEXT, password TEXT);"
+        };
+        
+        Statement s = conn.createStatement();
+        s.executeUpdate("CREATE DATABASE IF NOT EXISTS " + databaseName);
+        
+        conn = getConnection();
+        s = conn.createStatement();
+        
+        for(String tableSt : tableStatements){
+            s.executeUpdate("CREATE TABLE IF NOT EXISTS " + tableSt);
+        }
+
     }
 }
--- src/lockerbox/LockerBox.java
+++ src/lockerbox/LockerBox.java
@@ -3,6 +3,12 @@ package lockerbox;
 public class LockerBox {
 
     public static void main(String[] args) {
+        try {
+            DatabaseModel.initDatabase();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
         new LoginForm().setVisible(true);
     }
     
